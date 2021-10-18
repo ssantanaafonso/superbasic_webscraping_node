d1d9f085709431189e02900ad44dea38d87cec1b
Sergio Afonso <ssantanaafonso@gmail.com>
2021-10-18 21:15:21 +0100

    Async axios + cheerio

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const rawHtml = axios.get('https://news.ycombinator.com/jobs');
-rawHtml.then(
-    res => {
-        const $ = cheerio.load(res.data);
-        let text = [];
-        $('.titlelink').each(function (){
+let rawHtml = getPage('https://news.ycombinator.com/jobs');
+let text = [];
+let textFiltered = [];
+async function iterateOverMyBody(){
+    let res;
+    for (let i = 0; i < 5; i++) {
+        res = await rawHtml;
+        rawHtml = getPage(extractInfo(res.data));
+        
 
-            text.push($( this ).text());
-        });
-        console.log(text);
     }
-)
 
+    return text;
+}
+
+
+function getPage(page){
+    return axios.get(page);
+}
+
+function extractInfo(data){
+    const $ = cheerio.load(data);
+    $('.titlelink').each(function (){
+        text.push($( this ).text());
+    });
+    return `https://news.ycombinator.com/${$('.morelink').attr('href')}`;
+}
+
+iterateOverMyBody().then( text => {
+    console.log(text)
+    textFiltered = text.filter(el => el.toLowerCase().includes('front end'));
+    console.log(textFiltered);
+    console.log("ponme un reto");
+});
